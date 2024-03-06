f26902afd2a83cdbfc10c83c141a79ef5e79a238
Jose Angel Lugo <josealr97@gmail.com>
2024-03-05 23:12:49 -0500

    Most basic movement
    Player controller and basic movement, mouse rotation

---
+++ Assets/__Scripts/PlayerControl.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private Camera cam;
+    
+    private Vector2 inputVector = new Vector2();
+    private Quaternion targetRotation;
+    private Vector3 mousePosition;
+    void Update()
+    {
+        inputVector.x = Input.GetAxisRaw("Horizontal");
+        inputVector.y = Input.GetAxisRaw("Vertical");
+        if (inputVector.magnitude > 0)
+        {
+            inputVector.Normalize();
+            transform.Translate(inputVector * (walkSpeed * Time.deltaTime));
+        }
+        
+        var mouseVector = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -cam.transform.position.z);
+        mousePosition = Camera.main.ScreenToWorldPoint(mouseVector);
+
+        targetRotation = Quaternion.LookRotation(mousePosition - transform.position, Vector3.back);
+        targetRotation.x = 0.0f;
+        targetRotation.y = 0.0f;
+        
+        transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(mousePosition, 0.5f);
+    }
+}
